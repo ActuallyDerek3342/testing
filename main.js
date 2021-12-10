8796de4fb470dd62d59fea2e291be13d082d5926
ActuallyDerek3342 <72778853+ActuallyDerek3342@users.noreply.github.com>
2021-12-10 14:07:40 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 /* If there is an error then script will not run*/
 window.win = function() {
 	alert("Running");
-	window.location.href = "https://raw.githubusercontent.com/ActuallyDerek3342/testing/main/html.html";
-	window.open("https://www.google.com");
+	/*window.location.href = "https://raw.githubusercontent.com/ActuallyDerek3342/testing/main/html.html";
+	window.open("https://www.google.com");*/
+	
+	document.write('<html><body><h2>HTML</h2></body></html>');
 	
 	alert("END");
 };
