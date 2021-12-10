a09c8790c088a28c9e61b82b26dd581d533c20b6
ActuallyDerek3342 <72778853+ActuallyDerek3342@users.noreply.github.com>
2021-12-10 12:32:55 -0800

    Update vpn.js

--- vpn.js
+++ vpn.js
@@ -1,7 +1,5 @@
 
 window.win = function() {
 	alert("Running script!");
-	console.log("test");
-	console.log(window.open('https://www.google.com', '_blank'););
-	alert("Done with running.")
+	window.location.href = "html.html"
 };
