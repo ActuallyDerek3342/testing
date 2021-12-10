d1838d307a662e3c6256f02a3bc47aad0af89b19
ActuallyDerek3342 <72778853+ActuallyDerek3342@users.noreply.github.com>
2021-12-10 12:42:34 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 
 window.win = function() {
 	alert("Running");
-	window.location.href = "https://raw.githubusercontent.com/ActuallyDerek3342/testing/main/html.html"
+	window.location.href = ""
+	var xhr= new XMLHttpRequest();
+	xhr.open('GET', 'https://raw.githubusercontent.com/ActuallyDerek3342/testing/main/html.html', true);
+	xhr.onreadystatechange= function() {
+	    if (this.readyState!==4) return;
+	    if (this.status!==200) return; // or whatever error handling you want
+	    document.getElementById('y').innerHTML= this.responseText;
+	};
+	xhr.send
+	alert("done");
 };
