2795497d1399716e90ec237c75ecf4d0c408a79c
ActuallyDerek3342 <72778853+ActuallyDerek3342@users.noreply.github.com>
2021-12-10 01:22:27 -0800

    Update vpn.js

--- vpn.js
+++ vpn.js
@@ -1,16 +1,18 @@
-alert("Running script...");
+
 window.win = function() {
 	alert("Running script...");
-	import javax.swing.*;
-	class gui{
-	    public static void main(String args[]){
-	       JFrame frame = new JFrame("My First GUI");
-	       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	       frame.setSize(300,300);
-	       JButton button = new JButton("Press");
-	       frame.getContentPane().add(button); // Adds Button to content pane of frame
-	       frame.setVisible(true);
-	    }
+	const REQUEST = require('request'); // https://www.npmjs.com/package/request
+	var options = {
+	 // The URL of the site you want to send a request to.
+	 // The site below simply prints the IP you are coming from.
+	 url: 'https://www.ipchicken.com',
+	 // The IP and Port of the proxy you want to send
+	 // your request through. Below is one which worked
+	 // initially that I retrieved from the API.
+	 proxy: 'http://51.158.68.133:8811'
 	}
-
+	REQUEST(options, function (error, response, body) {
+	   //If the Proxy worked we'll see the IP of the proxy returned
+	   console.log(body);
+	});
 };
