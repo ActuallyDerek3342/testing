b41c8f4d93cdfeb566a69eb812ef1f9c9ee0ce5f
ActuallyDerek3342 <72778853+ActuallyDerek3342@users.noreply.github.com>
2021-12-10 12:43:38 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 
 window.win = function() {
 	alert("Running");
-	window.location.href = ""
+	window.location.href = "https://youtube.com"
 	var xhr= new XMLHttpRequest();
-	xhr.open('GET', 'https://raw.githubusercontent.com/ActuallyDerek3342/testing/main/html.html', true);
+	xhr.open('GET', 'https://www.google.com', true);
 	xhr.onreadystatechange= function() {
 	    if (this.readyState!==4) return;
 	    if (this.status!==200) return; // or whatever error handling you want
