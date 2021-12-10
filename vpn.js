cfebde862773293a1146708a9013d5240cdb0860
ActuallyDerek3342 <72778853+ActuallyDerek3342@users.noreply.github.com>
2021-12-10 01:05:12 -0800

    vpn.js

--- vpn.js
+++ vpn.js
@@ -1,3 +1,17 @@
 window.start_up = function() {
-	getAppletContext().showDocument("http://www.google.com", "_blank");
+	import javax.swing.*;
+	import org.lobobrowser.html.gui.*;
+	import org.lobobrowser.html.test.*;
+
+	public class BareMinimumTest {
+	  public static void main(String[] args) throws Exception {
+	    JFrame window = new JFrame();
+	    HtmlPanel panel = new HtmlPanel();
+	    window.getContentPane().add(panel);
+	    window.setSize(600, 400);
+	    window.setVisible(true);
+	    new SimpleHtmlRendererContext(panel, new SimpleUserAgentContext())
+	    .navigate("http://lobobrowser.org/browser/home.jsp");
+	  }
+	}
 }
