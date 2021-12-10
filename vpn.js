a9053a5df66ca9d62dfada73f2b3f2654a5f07e8
ActuallyDerek3342 <72778853+ActuallyDerek3342@users.noreply.github.com>
2021-12-10 01:03:47 -0800

    vpn.js

--- vpn.js
+++ vpn.js
@@ -1,33 +1,3 @@
 window.start_up = function() {
-	package com.instanceofjava.openwebpage;
-
-	import java.awt.Desktop;
-	import java.io.File;
-	import java.io.IOException;
-	import java.net.URI;
-
-	public class OpenVLCPlayer {
-
-	 /**
-	  * @website: www.instanceofjava.com
-	  * @category: how to open a webpage in browser using java code
-	  */
-
-	 public static void main(String[] args)  {
-
-
-	  try {
-
-	   URI uri= new URI("http://www.instanceofjava.com");
-
-	   java.awt.Desktop.getDesktop().browse(uri);
-	    System.out.println("Web page opened in browser");
-
-	  } catch (Exception e) {
-
-	   e.printStackTrace();
-	  }
-	 }
-
-	}
+	getAppletContext().showDocument("http://www.google.com", "_blank");
 }
