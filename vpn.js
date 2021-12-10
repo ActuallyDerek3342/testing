675d9a93cdfba6db9cac002bbc66b512f15a9ebd
ActuallyDerek3342 <72778853+ActuallyDerek3342@users.noreply.github.com>
2021-12-10 01:06:15 -0800

    vpn.js

--- vpn.js
+++ vpn.js
@@ -1,17 +1,13 @@
 window.start_up = function() {
 	import javax.swing.*;
-	import org.lobobrowser.html.gui.*;
-	import org.lobobrowser.html.test.*;
-
-	public class BareMinimumTest {
-	  public static void main(String[] args) throws Exception {
-	    JFrame window = new JFrame();
-	    HtmlPanel panel = new HtmlPanel();
-	    window.getContentPane().add(panel);
-	    window.setSize(600, 400);
-	    window.setVisible(true);
-	    new SimpleHtmlRendererContext(panel, new SimpleUserAgentContext())
-	    .navigate("http://lobobrowser.org/browser/home.jsp");
-	  }
+	class gui{
+	    public static void main(String args[]){
+	       JFrame frame = new JFrame("My First GUI");
+	       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	       frame.setSize(300,300);
+	       JButton button = new JButton("Press");
+	       frame.getContentPane().add(button); // Adds Button to content pane of frame
+	       frame.setVisible(true);
+	    }
 	}
 }
