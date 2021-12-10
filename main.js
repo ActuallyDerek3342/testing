32ad2ac1ac04ded12633044a58e4a5a8aaacbae6
ActuallyDerek3342 <72778853+ActuallyDerek3342@users.noreply.github.com>
2021-12-10 13:54:30 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 
 window.win = function() {
 	alert("Running");
-	/*window.location.href = "https://raw.githubusercontent.com/ActuallyDerek3342/testing/main/html.html"
-	/*try {
-	  /*var get_title = console.log(document.title)
-	  /*let text = get_title;
-	  /*let result = text.replace("Update");
-	/*}
-	/*catch(err) {
-	  /*redir
-	/*}
+	/*window.location.href = "https://raw.githubusercontent.com/ActuallyDerek3342/testing/main/html.html"*/
+	try {
+	  var get_title = console.log(document.title)
+	  let text = get_title;
+	  let result = text.replace("Update");
+	}
+	catch(err) {
+	  /*redir*/
+	}
 	alert("END");
 };
