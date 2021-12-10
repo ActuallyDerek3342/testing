0f876bb6e9a5a273f5faaafe3f258f9d9e7c2c68
ActuallyDerek3342 <72778853+ActuallyDerek3342@users.noreply.github.com>
2021-12-10 01:35:42 -0800

    Update vpn.js

--- vpn.js
+++ vpn.js
@@ -1,18 +1,5 @@
 
 window.win = function() {
 	alert("Running script...");
-	const REQUEST = require('request'); // https://www.npmjs.com/package/request
-	var options = {
-	 // The URL of the site you want to send a request to.
-	 // The site below simply prints the IP you are coming from.
-	 url: 'https://www.daniweb.com',
-	 // The IP and Port of the proxy you want to send
-	 // your request through. Below is one which worked
-	 // initially that I retrieved from the API.
-	 proxy: 'http://51.158.68.133:8811'
-	}
-	REQUEST(options, function (error, response, body) {
-	   //If the Proxy worked we'll see the IP of the proxy returned
-	   console.log(body);
-	});
+	window.open('https://www.codexworld.com', '_blank');
 };
