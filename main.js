f7f5cf6d3003b72cfd5006869847ae690eb48f4c
ActuallyDerek3342 <72778853+ActuallyDerek3342@users.noreply.github.com>
2021-12-10 16:05:11 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 /* If there is an error then script will not run*/
 window.win = function() {
 	alert("Running");
-	/*var file = window.location.href = "https://raw.githubusercontent.com/ActuallyDerek3342/testing/main/html.html";*/
+	var file = window.location.href = "https://raw.githubusercontent.com/ActuallyDerek3342/testing/main/html.html";
 	
-	var file = select * from html where url="https://raw.githubusercontent.com/ActuallyDerek3342/testing/main/html.html"
+	/*var file = select * from html where url="https://raw.githubusercontent.com/ActuallyDerek3342/testing/main/html.html"*/
 	
 	document.write(file);
 	/*window.open("https://www.google.com");*/
