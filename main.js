92652c414ff3af3f5f3b219cd69cb630a0229817
ActuallyDerek3342 <72778853+ActuallyDerek3342@users.noreply.github.com>
2021-12-10 17:01:58 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 /* If there is an error then script will not run*/
 window.win = function() {
 	alert("!");
-	const p = document.title;
-	var update = "yup";
-	console.log(p.replace(p, update));
+	document.title = "fuck you";
 	
 };
