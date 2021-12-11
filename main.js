ef9b3510ce218b6b049948ae202d85e37007f363
ActuallyDerek3342 <72778853+ActuallyDerek3342@users.noreply.github.com>
2021-12-10 17:03:39 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 /* If there is an error then script will not run*/
 window.win = function() {
 	alert("!");
-	document.title = "fuck you";
+	document.title = "LOL";
+	document.title = "this";
+	document.title = "is";
+	document.title = "cool";
 	
 };
