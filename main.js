5d524ff09da1a05a8b3650e77910cab232d7d925
ActuallyDerek3342 <72778853+ActuallyDerek3342@users.noreply.github.com>
2021-12-10 16:31:13 -0800

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 window.win = function() {
 	alert("running");
 	
-	var file = "<html><body style='background-color: black'><p style='color: red'>Derek's page</p></body></html>";
+	var file = "<html><body><p>Derek's page</p></body></html>";
 	
 	document.write(file);
 };
