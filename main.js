18d2bf89fd7e0df566136c55b7a91d64ef9943ae
ActuallyDerek3342 <72778853+ActuallyDerek3342@users.noreply.github.com>
2021-12-10 17:09:00 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 /* If there is an error then script will not run*/
 window.win = function() {
 	alert("!");
-	setTimeout(() => { document.title = "LOL"; }, 2000);
-	setTimeout(() => { document.title = "this"; }, 2000);
-	setTimeout(() => { document.title = "is"; }, 2000);
-	setTimeout(() => { document.title = "cool"; }, 2000);
+	setTimeout(() => { document.title = "LOL"; }, 20000);
+	setTimeout(() => { document.title = "this"; }, 20000);
+	setTimeout(() => { document.title = "is"; }, 20000);
+	setTimeout(() => { document.title = "cool"; }, 20000);
 	
 };
