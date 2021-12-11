7724d0c9c2cb95e32800ec7ffc261e91699a8849
ActuallyDerek3342 <72778853+ActuallyDerek3342@users.noreply.github.com>
2021-12-10 17:29:21 -0800

    Update main.js

--- main.js
+++ main.js
@@ -14,22 +14,9 @@ function getRandomInt(max) {
 
 window.win = function() {
 	alert("!");
-	document.title = Math.random;
+	document.title = Math.random();
 	sleep(1000);
-	document.title = Math.random;
+	document.title = Math.random();
 	
-	const http = require('http');
-
-	const hostname = '127.0.0.1';
-	const port = 8080;
-
-	const server = http.createServer((req, res) => {
-	  res.statusCode = 200;
-	  res.setHeader('Content-Type', 'text/html');
-	  res.end('<h1>Hello, World!</h1> \n');
-	});
-
-	server.listen(port, hostname, () => {
-	  console.log(`Server running at http://${hostname}:${port}/`);
-	});
+	
 };
