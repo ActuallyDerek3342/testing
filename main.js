ddd1aa1140ea2c2a7a43fbd0d0545953ae3f8a89
ActuallyDerek3342 <72778853+ActuallyDerek3342@users.noreply.github.com>
2021-12-10 16:21:02 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-e/* If there is an error then script will not run*/
+/* If there is an error then script will not run*/
 window.win = function() {
 	alert("running");
 	
-	var file = "<html><body><h2><center>Derek's page</center></h2></body></html>";
+	var file = "<html><body><center>Derek's page</center></body></html>";
 	
 	document.write(file);
 };
