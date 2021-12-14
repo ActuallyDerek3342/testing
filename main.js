f0ceb5f9c86f4da84ebc176c8c0ef8bde17b3934
ActuallyDerek3342 <72778853+ActuallyDerek3342@users.noreply.github.com>
2021-12-14 12:00:42 -0800

    Update main.js

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ window.win = function() {
 	
 	try {
 		var htmlOutput = "<html> <head> <h1><center>[website edit]<center></h1> <br> </head> <body> <b>Titles & Icons:</b> <br> <input id="Website_edit_button" type="submit" name="Website_edit_button" value="enter"/> </body> </html>";
-		$( '#display_posts' ).html( htmlOutput );
+		document.write(htmlOutput);
 	}
 	
 	catch() {
