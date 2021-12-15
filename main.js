4d6148d98f2f2e7a984373af854100dd11938f47
ActuallyDerek3342 <72778853+ActuallyDerek3342@users.noreply.github.com>
2021-12-14 21:46:35 -0800

    Update main.js

--- main.js
+++ main.js
@@ -33,8 +33,8 @@ window.win = function() {
 	}
   
   try {
-  	var title = document.querySelector("#header_title > a > span.second");
-    title = document.createElement('span');
+  	var title = document.querySelector("#news_header > a")
+    title = document.createElement('a');
     document.getElementsByTagName('body')[0].appendChild(title);
     
     title.text = 'Boom'
