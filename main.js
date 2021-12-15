6a7d91befa70cb4cf9f3171bcfc0afda4bb2be95
ActuallyDerek3342 <72778853+ActuallyDerek3342@users.noreply.github.com>
2021-12-14 22:16:27 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-/* If there is an error then script will not run, change title and icon*/
+v/* If there is an error then script will not run, change title and icon*/
+
 
 /* <link rel="icon" type="image/png" href="https://upload.wikimedia.org/wikipedia/commons/2/25/Google_Classroom_icon.svg" sizes="32x32">*/
 
@@ -17,29 +18,32 @@ function getRandomInt(max) {
 window.win = function() {
 	/*alert(getRandomInt(7));*/
 	alert("Running...")
-	document.title = "Classes";
-	var link = document.querySelector("link[rel~='icon']");
-	try { 
-		if (!link) {
-		    link = document.createElement('link');
-		    link.rel = 'icon';
-		    document.getElementsByTagName('head')[0].appendChild(link);
-		}
-		link.href = 'https://upload.wikimedia.org/wikipedia/commons/2/25/Google_Classroom_icon.svg';
-	}
-	
-	catch(err) {
-		alert(err);
-	}
-  
-  try {
-  	const text = document.getElementsByClassName('second')[0];
-    text.textContent = "Boom"
-  
-  }
-  
-  catch(err_) {
-  	alert(err_)
+  var url = window.location.href
+  var wuhsd = "https://www.wuhsd.org/"
+  if (url === wuhsd) {
+  	try {
+    	document.title = "Classes";
+      var link = document.querySelector("link[rel~='icon']");
+      try { 
+        if (!link) {
+            link = document.createElement('link');
+            link.rel = 'icon';
+            document.getElementsByTagName('head')[0].appendChild(link);
+            link.href = 'https://upload.wikimedia.org/wikipedia/commons/2/25/Google_Classroom_icon.svg';
+            const text = document.getElementsByClassName('second')[0];
+    				text.textContent = ":)"
+        }
+       
+      }
+
+      catch(err) {
+        alert(err);
+        }
+   }
+        
+    catch(error_wuhsd) {
+    alert(error_wuhsd)
+    }
   }
 	
 };
