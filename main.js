069054fb507a522995103c7678a9ab908a97f76a
ActuallyDerek3342 <72778853+ActuallyDerek3342@users.noreply.github.com>
2021-12-17 12:30:02 -0800

    Update main.js

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ function getRandomInt(max) {
 
 window.win = function() {
 	/*alert(getRandomInt(7));*/
-	var added = "+ 'html'";
+	var added = "+ 'alert'";
 	alert("Running...[" + added + "]")
   	var url = window.location.href
 	document.title = "Classes";
