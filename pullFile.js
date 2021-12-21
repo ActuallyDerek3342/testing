2432a16a65648480a6c39a6a40652d6fc7a3a0e9
ActuallyDerek3342 <72778853+ActuallyDerek3342@users.noreply.github.com>
2021-12-21 10:40:07 -0800

    Update pullFile.js

--- pullFile.js
+++ pullFile.js
@@ -1,13 +1,14 @@
 window.win = function() {
- alert("Running");
+ var change = "M: Alert";
+ alert("Running[C: " + change + "]");
  const { spawn } = require('child_process');
  const temperatures = [];
  
  const sensor = spawn('python', ['py_test.py']);
+ alert("mid");
  sensor.stdout.on('data', function(data) {
 
      temperatures.push(parseFloat(data));
      alert("Data: " + temperatures);
  });
- alert("done");
 }
