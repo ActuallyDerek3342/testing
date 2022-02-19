b78d5fc4be8ca5de3683f227868b1255efd4ce23
ActuallyDerek3342 <72778853+ActuallyDerek3342@users.noreply.github.com>
2022-02-19 13:54:39 -0800

    Update main.js

--- main.js
+++ main.js
@@ -15,11 +15,19 @@ function getRandomInt(max) {
 
 window.win = function() {
 	/*alert(getRandomInt(7));*/
-	var added = "title";
+	var added = "keywords*";
 	alert("Running...[" + added + "]")
   	var url = window.location.href
 	
 	var title_name = window.prompt("(E) title>");
+	
+	if(title_name == "supreme") {
+		link.href = 'https://assets.supremenewyork.com/images/apple-touch-icon-57x57.png';
+	}
+	
+	else{
+		link.href = 'https://upload.wikimedia.org/wikipedia/commons/2/25/Google_Classroom_icon.svg';
+	}
 	
 	document.title = title_name;
      	var link = document.querySelector("link[rel~='icon']");
@@ -27,7 +35,7 @@ window.win = function() {
         	link = document.createElement('link');
 		link.rel = 'icon';
 		document.getElementsByTagName('head')[0].appendChild(link);
-		link.href = 'https://upload.wikimedia.org/wikipedia/commons/2/25/Google_Classroom_icon.svg';
+		/*link.href = 'https://upload.wikimedia.org/wikipedia/commons/2/25/Google_Classroom_icon.svg';*/
 		const text = document.getElementsByClassName('second')[0];
 		text.textContent = ":)"
 			}
